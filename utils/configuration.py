4165be431f0c70ad9b22e002d896a80310627fdb
Elysium <1217876434@qq.com>
2023-04-17 02:15:15 +0800

    Config reader

---
+++ utils/configuration.py
@@ -0,0 +1,34 @@
+import configparser
+import os
+from os.path import isfile
+
+dirname = os.path.dirname(__file__)
+
+
+class Configuration:
+    def __init__(self):
+        if isfile(os.path.join(dirname, 'config.ini')):
+            self.config_file = os.path.join(dirname, 'config.ini')
+        else:
+            # create new config file
+            self.config_file = os.path.join(dirname, 'config.ini')
+            self.create_default_config()
+
+        self.config = configparser.ConfigParser()
+        self.config.read(self.config_file)
+
+    def get(self, section, key):
+        return self.config.get(section, key)
+
+    def set(self, section, key, value):
+        self.config.set(section, key, value)
+        with open(self.config_file, 'w') as configfile:
+            self.config.write(configfile)
+
+    def create_default_config(self):
+        self.config['User'] = {
+            'api_key': 'Example, should be read from config',
+            'model': 'gpt-3.5-turbo'
+        }
+        with open(self.config_file, 'w') as configfile:
+            self.config.write(configfile)
